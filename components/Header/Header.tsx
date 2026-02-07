ba2e3f2354eb1f0a75010984d3774a26379b819d
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-02-07 20:08:50 +0200

    add Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <header>
+            <a href="/" aria-label="Home">
+                NoteHub
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Notes</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
